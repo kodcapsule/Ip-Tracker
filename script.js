adc90fe71e971ff100250c4ef835b8f15f8f091e
kuseh-wewoli <kusehsw@gmail.com>
2022-08-23 15:46:28 +0000

    Changes to javaScript file

--- script.js
+++ script.js
@@ -11,54 +11,6 @@ const locationCity = document.querySelector(".locattion");
 const locationTimeZone = document.querySelector(".time-zone");
 const isp = document.querySelector(".isp");
 
-// class IpTracker {
-//   // class fields
-//   #ipAddress = "";
-//   #coordinates;
-//   #domains;
-//   #region = "";
-//   #country = "";
-//   #city = "";
-//   #timezone = "";
-//   #isp = "";
-
-//   constructor(ip = "8.8.8.8", apikey = apiKey) {
-//     this.apikey = apikey;
-//     this.ip = ip;
-//   }
-
-//   async getIpDetails() {
-//     let url = "https://geo.ipify.org/api/v2/country,city,vpn?apiKey=";
-//     let http = await fetch(`${url}${this.apikey}&ipAddress=${this.ip}`);
-//     ipData = await http.json();
-//     this.#ipAddress = ipData.ip;
-//     const {
-//       region: region,
-//       country: country,
-//       city: city,
-//       lat: lat,
-//       lng: lng,
-//       timezone: timezone,
-//     } = ipData.location;
-//     this.#coordinates = [lat, lng];
-//     this.#domains = ipData.domains;
-//     this.#region = region;
-//     this.#country = country;
-//     this.#city = city;
-//     this.#timezone = timezone;
-//     this.#isp = ipData.isp;
-//   }
-
-//   async renderMap() {
-//     let map = L.map("map").setView(this.#coordinates, 15);
-//     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//       maxZoom: 19,
-//       attribution: "© OpenStreetMap",
-//     }).addTo(map);
-//     var marker = L.marker(this.#coordinates).addTo(map);
-//   }
-// }
-
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   ipAddress = input_ip.value;
@@ -106,32 +58,3 @@ function renderMap(lat, lng, zoom = 13) {
   }).addTo(map);
   var marker = L.marker([lat, lng]).addTo(map);
 }
-
-// const data = getIpDetails("8.8.8.8");
-// (async () => {
-//   const { location: loc } = await getIpDetails("102.134.130.0");
-//   renderMap(loc.lat, loc.lng);
-//   console.log(loc);
-// })();
-
-// console.log(data);
-// console.log(data.then(data) => data.as);
-// const ipDetails = data
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-// console.log(ipDetails);
-
-// navigator.geolocation.getCurrentPosition((pos) => {
-//   let { latitude: lat, longitude: log } = pos.coords;
-//   console.log(lat, log);
-//   let map = L.map("map").setView([lat, log], 15);
-//   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-//     maxZoom: 19,
-//     attribution: "© OpenStreetMap",
-//   }).addTo(map);
-//   var marker = L.marker([lat, log]).addTo(map);
-// });
-
-// const ipt = new IpTracker(ipAddress, apiKey);
-// ipt.getIpDetails();
-// ipt.renderMap();
